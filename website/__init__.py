adf47768de8ad9080f8dfb05267d2848834a9d91
mohamedemad11 <mohamedemad353@gmail.com>
2023-12-08 19:00:23 +0200

    Adding Some Functions

--- website/__init__.py
+++ website/__init__.py
@@ -1,6 +1,9 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from os import path 
+from flask_login import LoginManager
+
+
 db = SQLAlchemy()
 DB_NAME= 'database.db'
 
@@ -13,6 +16,8 @@ def create_app():
     
     from .views import views 
     from .auth import auth
+
+
     app.register_blueprint(views , url_prefix='/')
     app.register_blueprint(auth , url_prefix='/')
 
@@ -21,7 +26,17 @@ def create_app():
 
 
 
-    create_database(app)
+    with  app.app_context():
+        db.create_all()
+
+    login_manager = LoginManager()
+    login_manager.login_view='auth.login'
+    login_manager.init_app(app)
+
+    
+    @login_manager.user_loader
+    def load_user(id) :
+        return User.query.get(int(id))
     return app
 
 
--- website/auth.py
+++ website/auth.py
@@ -1,38 +1,65 @@
-from flask import Blueprint , render_template ,request ,flash
-
+from flask import Blueprint , render_template ,request ,flash , redirect, url_for
+from .models import User
+from werkzeug.security import generate_password_hash , check_password_hash
+from . import db
+from flask_login import login_user , login_required , logout_user , current_user
 auth = Blueprint('auth',__name__)
 
 
 @auth.route('/login',methods=['GET','POST'])
 def login():
-    
-    return render_template('login.html')
+    if request.method =='POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+        user = User.query.filter_by(email=email).first()
+        if user :
+            if check_password_hash(user.password,password):
+                flash('Logged in successfully!',category='success')
+                login_user(user,remember= True)
+                return redirect(url_for('views.home'))
+            else :
+                flash('Incorrect password , try again.', category='error')
+        else :
+            flash('Email does not exist.' ,category='error')
+        
+    return render_template('login.html', user = current_user)
 
 @auth.route('/logout')
+@login_required
 def logout():
-    return render_template('logout.html')
+    logout_user()
+    return redirect(url_for('auth.login'))
 
 
 @auth.route('/SignUp',methods= ['GET','POST'])
 def sign_up():
     if request.method =='POST':
         email = request.form.get('email')
-        firstname= request.form.get('firstName')
-        lastname = request.form.get('lastName')
+        first_name= request.form.get('firstName')
+        last_name = request.form.get('lastName')
         password1 = request.form.get('password1')
         password2= request.form.get('password2')
-        if len(email) < 4 :
+        user = User.query.filter_by(email=email).first()
+        if user :
+            flash('Email already Exists.', category='error')
+        elif len(email) < 4 :
             flash('Email Must be larger than 4 characters !',category='error') 
-        elif len(firstname) < 2 :
+        elif len(first_name) < 2 :
             flash('First Name Must be larger than 4 characters !',category='error')
-        elif len(lastname) < 2 :
+        elif len(last_name) < 2 :
             flash('Last Name must be more than 4 characters  !',category='error')
         elif password1 != password2 :
             flash('Passwords Don\'t Match !',category='error')
         elif len(password1) < 7 :
             flash('The Password must be more than 6 !',category='error')
         else :
+            
+            new_user = User(email=email, first_name=first_name , last_name= last_name, password =generate_password_hash(password1,method='pbkdf2:sha1') )
+            db.session.add(new_user)
+            db.session.commit()
+            login_user(new_user,remember= True)
             flash('Account Created Successfully !' ,category='success')
-    return render_template('signup.html',text = "Hello Hard Worker ! :) ")
+            return redirect(url_for('views.home'))
+    return render_template('signup.html', user= current_user)
 
 
